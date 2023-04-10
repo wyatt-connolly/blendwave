b48480617e56e66d1d9ed96f08388ea468c45385
MassyWeeWee <wyattconnollyfitness@gmail.com>
2023-04-09 20:25:58 -0700

    init

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { ClerkProvider } from '@clerk/nextjs/app-beta'
+
+import Footer from '@/components/layout/footer'
+import Header from '@/components/layout/header'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin']
+})
+
+export const metadata = {
+  title: 'BlendWave',
+  description: 'Created by Wyatt Connolly'
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html
+      lang='en'
+      className={`${inter.className} h-full scroll-smooth antialiased`}
+    >
+      <body className='flex h-full flex-col text-stone-700'>
+        <ClerkProvider>
+          <Header />
+          <main className='grow'>{children}</main>
+          <Footer />
+        </ClerkProvider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
